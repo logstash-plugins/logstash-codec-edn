dfbe2c6fe6f9782a386a2868b1bc9e9975807ed0
kares <self@kares.org>
2021-07-28 12:14:33 +0200

    Feat: target configuration + event-factory support

--- lib/logstash/codecs/edn.rb
+++ lib/logstash/codecs/edn.rb
@@ -1,9 +1,22 @@
 require "logstash/codecs/base"
 require "logstash/util"
 
+require 'logstash/plugin_mixins/event_support/event_factory_adapter'
+require 'logstash/plugin_mixins/validator_support/field_reference_validation_adapter'
+
 class LogStash::Codecs::EDN < LogStash::Codecs::Base
+
+  extend LogStash::PluginMixins::ValidatorSupport::FieldReferenceValidationAdapter
+
+  include LogStash::PluginMixins::EventSupport::EventFactoryAdapter
+
   config_name "edn"
 
+  # Defines a target field for placing decoded fields.
+  # If this setting is omitted, data gets stored at the root (top level) of the event.
+  #
+  # NOTE: the target is only relevant while decoding data into a new event.
+  config :target, :validate => :field_reference
 
   def register
     require "edn"
@@ -12,10 +25,10 @@ class LogStash::Codecs::EDN < LogStash::Codecs::Base
   public
   def decode(data)
     begin
-      yield LogStash::Event.new(EDN.read(data))
+      yield targeted_event_factory.new_event(EDN.read(data))
     rescue => e
       @logger.warn("EDN parse failure. Falling back to plain-text", :error => e, :data => data)
-      yield LogStash::Event.new("message" => data)
+      yield event_factory.new_event("message" => data)
     end
   end
 
