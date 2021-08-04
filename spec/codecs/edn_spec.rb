96e1d6d33c1edc7f16c0541e17dffd90d0447596
Karol Bucek <kares@users.noreply.github.com>
2021-08-04 15:37:08 +0200

    Feat: target configuration + event-factory support (#6)

--- spec/codecs/edn_spec.rb
+++ spec/codecs/edn_spec.rb
@@ -6,26 +6,48 @@ require "insist"
 require "edn"
 
 describe LogStash::Codecs::EDN do
-  subject do
-    next LogStash::Codecs::EDN.new
-  end
+
+  subject { LogStash::Codecs::EDN.new(config) }
+
+  let(:config) { Hash.new }
+
+  let(:data) { {"foo" => "bar", "baz" => {"bah" => ["a", "b", "c"]}, "@timestamp" => "2014-05-30T02:52:17.929Z"} }
 
   context "#decode" do
     it "should return an event from edn data" do
-      data = {"foo" => "bar", "baz" => {"bah" => ["a", "b", "c"]}, "@timestamp" => "2014-05-30T02:52:17.929Z"}
+      event_count = 0
       subject.decode(data.to_edn) do |event|
+        event_count += 1
         insist { event }.is_a?(LogStash::Event)
         insist { event.get("foo") } == data["foo"]
         insist { event.get("baz") } == data["baz"]
         insist { event.get("bah") } == data["bah"]
         insist { event.get("@timestamp").to_iso8601 } == data["@timestamp"]
       end
+      expect(event_count).to eql 1
+    end
+
+    context 'with target' do
+
+      let(:config) { super().merge('target' => '[doc]') }
+
+      it "decodes an event" do
+        event_count = 0
+        subject.decode(data.to_edn) do |event|
+          event_count += 1
+          expect(event.include?("foo")).to be false
+          expect(event.get("[doc][foo]")).to eql 'bar'
+          expect(event.get("[doc][baz]")).to eql "bah" => ["a","b","c"]
+          expect(event.get("[doc][@timestamp]")).to eql data["@timestamp"]
+        end
+        expect(event_count).to eql 1
+      end
+
     end
   end
 
   context "#encode" do
     it "should return edn data from pure ruby hash" do
-      data = {"foo" => "bar", "baz" => {"bah" => ["a","b","c"]}, "@timestamp" => "2014-05-30T02:52:17.929Z"}
       event = LogStash::Event.new(data)
       got_event = false
       subject.on_event do |e, d|
@@ -43,7 +65,6 @@ describe LogStash::Codecs::EDN do
     # deserialization using JrJackson in :raw mode which creates Java LinkedHashMap
     # and not Ruby Hash which will not be monkey patched with the #to_edn method
     it "should return edn data from deserialized json with normalization" do
-      data = LogStash::Json.load('{"foo": "bar", "baz": {"bah": ["a","b","c"]}, "@timestamp": "2014-05-30T02:52:17.929Z"}')
       event = LogStash::Event.new(data)
       got_event = false
       subject.on_event do |e, d|
